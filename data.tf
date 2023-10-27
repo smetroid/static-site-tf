9f3efa8104533afaa3beb489a5e8df91a196f198
EnriqueC <enrique.carranco@kaiahealth.com>
2023-10-26 18:02:18 -0600

    adding data sources for the s3 bucket example

--- data.tf
+++ data.tf
@@ -1,3 +1,6 @@
+data "aws_canonical_user_id" "current" {}
+
+data "aws_cloudfront_log_delivery_canonical_user_id" "cloudfront" {}
 data "aws_cloudfront_cache_policy" "cache_policy" {
   for_each = local.config.cdn
   name     = each.value.cache_policy_name
