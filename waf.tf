684f36a73224466b30cd1673816a5a792167ae58
EnriqueC <enrique.carranco@kaiahealth.com>
2023-10-28 16:35:49 -0600

    working terraform waf configuration

--- waf.tf
+++ waf.tf
@@ -4,14 +4,14 @@ resource "aws_wafv2_ip_set" "white_list" {
   #count = local.config["wafv2"] == {} ? 0 : 1
   name               = "${each.key}-white-list"
   description        = "${each.key} white list"
-  scope              = "REGIONAL"
+  scope              = "CLOUDFRONT"
   ip_address_version = "IPV4"
 
   addresses = each.value.white_list
 }
 
 
-module "waf-webaclv2" {
+module "waf_webaclv2" {
   source  = "umotif-public/waf-webaclv2/aws"
   version = "5.1.2"
 
@@ -19,7 +19,7 @@ module "waf-webaclv2" {
   name_prefix            = each.key
   description            = "Web ACL for ${each.key} ${local.region} ${local.env}"
   allow_default_action   = true
-  scope                  = "REGIONAL"
+  scope                  = "CLOUDFRONT"
   create_alb_association = false
   visibility_config = {
     cloudwatch_metrics_enabled = false
@@ -34,13 +34,22 @@ module "waf-webaclv2" {
       action   = "allow"
       visibility_config = {
         cloudwatch_metrics_enabled = true
-        metric_name                = "${each.key}-${local.region}-${local.env}-white-lis"
+        metric_name                = "${each.key}-${local.region}-${local.env}-white-list"
         sampled_requests_enabled   = true
       }
       ip_set_reference_statement = {
         arn = aws_wafv2_ip_set.white_list[each.key].arn
       }
     },
+    #{
+    #  name     = "${each.key}-${local.region}-${local.env}-block-all"
+    #  priority = "1"
+    #  action   = "block"
+    #  visibility_config = {
+    #    cloudwatch_metrics_enabled = true
+    #    metric_name                = "${each.key}-${local.region}-${local.env}-blocks"
+    #    sampled_requests_enabled   = true
+    #  }
+    #},
   ]
 }
-
